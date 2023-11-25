62db5aa426a2e395c14d216fec113cf7284bb095
Luth-Badila <FrancescoLutfi10@gmail.com>
2023-11-25 20:24:54 +0700

    added unperfect card and undone

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { ButtonComponent } from "./components/Button";
 import { Home } from "./components/Home";
+import Card from "./components/Card";
 
 import "./App.css";
 
 function App() {
   return (
     <>
-      <Home />
+      <Card />
+      {/* <Home />
       <h1>This Is My Portofolio</h1>
-      <ButtonComponent />
+      <ButtonComponent /> */}
     </>
   );
 }
